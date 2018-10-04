29ae4d5b1d13db529ec20decd83bf4ee778726bc
Josh Bryant <bryantjm@wfu.edu>
2018-10-03 22:46:33 -0400

    Writing sql query:

--- hestia/page-submit-attendance.php
+++ hestia/page-submit-attendance.php
@@ -2,4 +2,23 @@
 // Include Youth Directory functions file
 require_once dirname(__FILE__ ) . '/youth-directory/functions.php';
 
-echo_pre($_GET,1);
+// Make sure that we have what we need before we try to write to attendance table
+if (!isset($_GET['attendance']) || $_GET['event'] === 'Choose One') {
+    die;
+}
+
+// Setting the stage
+global $wpdb;
+$table_name = $wpdb->prefix . 'attendance';
+$attendees = $_GET['attendance'];
+$date = $_GET['date'];
+$event = $_GET['event'];
+
+foreach ($attendees as $attendee) {
+    $data = array(
+        'attend_date' => $date,
+        'attend_event' => $event,
+        'attend_id' => $attendee
+    );
+    $wpdb->insert($table_name, $data);
+}
