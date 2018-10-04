217b94bac11ec1cc727a5066ac43227850e5e856
Josh Bryant <bryantjm@wfu.edu>
2018-10-03 22:49:32 -0400

    Displaying attendee information in an easy-to-read format

--- hestia/page-submit-attendance.php
+++ hestia/page-submit-attendance.php
@@ -13,7 +13,9 @@ $table_name = $wpdb->prefix . 'attendance';
 $attendees = $_GET['attendance'];
 $date = $_GET['date'];
 $event = $_GET['event'];
+$attendee_total = count($attendees);
 
+// Writing attendance to the database
 foreach ($attendees as $attendee) {
     $data = array(
         'attend_date' => $date,
@@ -22,3 +24,27 @@ foreach ($attendees as $attendee) {
     );
     $wpdb->insert($table_name, $data);
 }
+
+// Default Hestia information
+get_header();
+do_action('hestia_before_single_post_wrapper');
+$default = hestia_get_blog_layout_default();
+$sidebar_layout = apply_filters('hestia_sidebar_layout', get_theme_mod('hestia_blog_sidebar_layout', $default));
+$wrap_class = apply_filters('hestia_filter_single_post_content_classes', 'col-md-8 single-post-container');
+?>
+
+<div class="<?php echo hestia_layout(); ?>">
+    <div class="blog-post blog-post-wrapper">
+        <div class="container">
+                <article id="post-<?php the_ID(); ?>" class="section section-text">
+                    <div class="row">
+                        <div class="single-post-wrap youth-directory-wrap">
+                            <p>Today's attendance: <?php echo $attendee_total; ?></p>
+                        </div>
+                    </div>
+                </article>
+        </div>
+    </div>
+</div>
+<div class="footer-wrapper">
+    <?php get_footer(); ?>
